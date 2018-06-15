a052861f3a72f6a3d10db2145ab0237cd7c0cda1
Steven <saltysteven9@gmail.com>
2018-06-15 01:16:02 -0700

    added i love you and i hate you response to I.js

--- commands/i.js
+++ commands/i.js
@@ -1,10 +1,20 @@
  exports.run = (client, message, args) => {
-    if (args[0].toLowerCase() === "sample") {
-        //
+    if (args[0].toLowerCase() === "love you") {
+        // When the user says a variation of "love you" mikuru gives a random response
+        let temp = ["Awwww, thank you~", "I love you too~", "Watashi mo kimi o aishitemasu~"];
         message.channel.startTyping();
         setTimeout(() => {
-            // put response here
+            message.reply(temp[Math.floor(Math.random() * temp.length)]);
             message.channel.stopTyping();
-        }, 2000);
+        }, 3000);
+    };
+    if (args[0].toLowerCase() === "hate you") {
+        // When the user says a variation of "hate you" mikuru gives a random response
+        let temp = ["(´；Д；`)", ".°(ಗдಗ。)°.", "*Crying*"];
+        message.channel.startTyping();
+        setTimeout(() => {
+            message.reply(temp[Math.floor(Math.random() * temp.length)]);
+            message.channel.stopTyping();
+        }, 1000);
     };
  };
