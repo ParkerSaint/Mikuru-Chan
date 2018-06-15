e3d3ee5af6b617fc58b2eefe4a14f0af2dff6291
Parker St. Pierre <parker@parkersaint.com>
2018-06-14 15:22:14 -1000

    added "hello" command

--- index.js
+++ index.js
@@ -81,6 +81,12 @@ client.on("message", async message => {
         }, 2000);
     }
 
+    if (messageTrimmed.toLowerCase() === "hello") {
+        // When the user says a variation of hello Mikuru gives a random response
+        let temp = ["Hiya!", "Hello!", "ohayou, konnichiwa, konbanwa"]; // These are the three time of day based responses.   
+        message.reply(temp[Math.floor(Math.random() * temp.length)]);
+    }
+
 });
 
 client.login(config.token);
