e2d50483205d17cd001c13dcd65a37159d93400e
Parker St. Pierre <parker@parkersaint.com>
2018-06-14 23:30:18 -1000

    Formated "how are you"

--- commands/how.js
+++ commands/how.js
@@ -10,7 +10,12 @@ exports.run = (client, message, args) => {
 
     if (args.join(" ").toLowerCase() === "are you") {
         // When the user says a variation of "love you" mikuru gives a random response
-        let temp = ["I'm good, thanks~ （⌒▽⌒ゞ", "I'm doing pretty well today.", "Daijoubu~", "I'm feeling a bit better than usual. ໒( ͡ᵔ ▾ ͡ᵔ )७", "I'm good, how are you? （‐＾▽＾‐）", "I'm good, I guess. (*´-｀*)", "I'm feeling under the weather.", "I'm really angry right now. ( ╬◣ 益◢)", "I'm not fine at all. 。・°°・(＞_＜)・°°・。" ];
+        let temp = [
+            "I'm good, thanks~ （⌒▽⌒ゞ", "I'm doing pretty well today.", "Daijoubu~",
+            "I'm feeling a bit better than usual. ໒( ͡ᵔ ▾ ͡ᵔ )७", "I'm good, how are you? （‐＾▽＾‐）",
+            "I'm good, I guess. (*´-｀*)", "I'm feeling under the weather.", "I'm really angry right now. ( ╬◣ 益◢)",
+            "I'm not fine at all. 。・°°・(＞_＜)・°°・。"
+        ];
         message.channel.startTyping();
         setTimeout(() => {
             message.reply(temp[Math.floor(Math.random() * temp.length)]);
