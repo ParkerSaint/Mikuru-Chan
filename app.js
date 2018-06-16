d129d83e58de713841cc37603c47665746bc2777
Steven <saltysteven9@gmail.com>
2018-06-16 06:30:16 -0700

    changed ready state

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const config = require("./config.json");
 client.on("ready", () => {
     // This event will run if the bot starts, and logs in, successfully.
     console.log(`Mikuru-Chan has started.`);
-    client.user.setActivity(`Somthing Normal`);
+    client.user.setActivity(`with the lolis :3`);
 });
 
 client.on("message", async message => {
